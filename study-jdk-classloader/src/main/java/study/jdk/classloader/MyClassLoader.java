a8bd0349be70b3e01470eb50639460b6ef9263f8
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-09-04 16:07:35 +0800

    更新代码

--- study-jdk-classloader/src/main/java/study/jdk/classloader/MyClassLoader.java
+++ study-jdk-classloader/src/main/java/study/jdk/classloader/MyClassLoader.java
@@ -1,9 +1,45 @@
 package study.jdk.classloader;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
 public class MyClassLoader extends ClassLoader {
+
+    private String rootDir;
+
+    public MyClassLoader(String rootDir) {
+        this.rootDir = rootDir;
+    }
 
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-        return super.loadClass(name);
+        byte[] classData = getClassData(name);
+        if (classData == null) {
+            throw new ClassNotFoundException();
+        }
+        return defineClass(name, classData, 0, classData.length);
+    }
+
+    private byte[] getClassData(String className) {
+        String path = classNameToPath(className);
+        try {
+            InputStream ins = new FileInputStream(path);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int bufferSize = 4096;
+            byte[] buffer = new byte[bufferSize];
+            int bytesNumRead = 0;
+            while ((bytesNumRead = ins.read(buffer)) != -1) {
+                baos.write(buffer, 0, bytesNumRead);
+            }
+            return baos.toByteArray();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
---
+++ study-jdk-classloader/src/test/java/test/study/jdk/classloader/MyClassLoaderTest.java
@@ -0,0 +1,27 @@
+package test.study.jdk.classloader;
+
+import org.junit.Test;
+import study.jdk.classloader.MyClassLoader;
+
+import java.lang.reflect.Method;
+
+public class MyClassLoaderTest {
+
+    @Test
+    public void test(){
+        String classDataRootPath = "C:\\workspace\\Classloader\\classData";
+        MyClassLoader fscl1 = new MyClassLoader(classDataRootPath);
+        MyClassLoader fscl2 = new MyClassLoader(classDataRootPath);
+        String className = "com.example.Sample";
+        try {
+            Class<?> class1 = fscl1.loadClass(className);
+            Object obj1 = class1.newInstance();
+            Class<?> class2 = fscl2.loadClass(className);
+            Object obj2 = class2.newInstance();
+            Method setSampleMethod = class1.getMethod("setSample", java.lang.Object.class);
+            setSampleMethod.invoke(obj1, obj2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
