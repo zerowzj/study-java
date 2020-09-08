6330b20f25d37e8fb467b9d91b81edde07c1fa88
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-09-08 14:22:07 +0800

    更新代码

--- study-jvm/src/main/java/study/jvm/memory/HeapTest.java
+++ study-jvm/src/main/java/study/jvm/memory/HeapTest.java
@@ -1,4 +1,18 @@
 package study.jvm.memory;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.info.GraphLayout;
+import study.jvm.object.Obj;
+
+@Slf4j
 public class HeapTest {
+
+    @Test
+    public void obj_test() throws Exception {
+        Obj obj = new Obj();
+        GraphLayout.parseInstance("ABC").toImage("d:\\t.jpg");
+//        log.info(str);
+    }
 }
