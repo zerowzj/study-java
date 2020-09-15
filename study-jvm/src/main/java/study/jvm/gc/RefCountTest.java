18c5fb56ff05687e1e9ab82d1e34d06badb2e91c
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-09-15 14:41:31 +0800

    更新代码

---
+++ study-jvm/src/main/java/study/jvm/gc/RefCountTest.java
@@ -0,0 +1,23 @@
+package study.jvm.gc;
+
+import org.junit.Test;
+
+public class RefCountTest {
+
+    private static final int _1MB = 1024 * 1024;
+
+    public Object instance = null;
+
+    private byte[] bigSize = new byte[2 * _1MB];
+
+    @Test
+    public void test(){
+        RefCountTest objA  = new RefCountTest();
+        RefCountTest objB  = new RefCountTest();
+
+        objA.instance = objB;
+        objB.instance = objA;
+
+        System.gc();
+    }
+}
